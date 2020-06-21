ef933f30bafa556b80bb1c576046d2474c169713
EmmanuelPineiro <pineiroemmanuel@gmail.com>
2020-06-20 17:27:03 -0700

    added function docs and updated README (#1)
    * added function docs and updated README
    
    * added appveyor yaml
    
    * changed appveyor config extension
    
    * changed appveyor extension
    
    * changed appveyor config build to off
    
    * used appveryor generated yaml
    
    * dded additional testing to appveyor
    
    * added build badge
    
    * added test line 5 to appveyor build
    
    * updated appveyor.yml

--- PoShLumberjack/PoshLumberjack.psm1
+++ PoShLumberjack/PoshLumberjack.psm1
@@ -3,21 +3,42 @@ $script:LevelColors = @{INFO='White'; WARN='Yellow'; ERROR='Red'; DEBUG='Cyan'}
 $script:IsDebug = $false
 
 #region accessor functions
+<#
+.DESCRIPTION
+Set the text color write-host uses for a specific log level (i.e. INFO, WARN, ERROR, DEBUG)
+
+.PARAMETER Level
+The log level to set the color for
+Valid options: "INFO", "WARN", "ERROR", "DEBUG"
+
+.PARAMETER Color
+The Color to use for the specified log level text
+Valid options: "Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow",
+  "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White"
+.EXAMPLE
+Set-LevelTextColor "INFO" "Green"
+#>
 function Set-LevelTextColor {
   param (
     [Parameter(Mandatory=$true, Position=0)]
     [ValidateSet("INFO", "WARN", "ERROR", "DEBUG")]
     [String]
-    $tag,
+    $Level,
     [Parameter(Mandatory=$true, Position=1)]
     [ValidateSet("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")]
     [String]
-    $color
+    $Color
   )
-  $script:LevelColors.$tag = $color
-  Write-Host "Set $tag to $color"
+  $script:LevelColors.$Level = $Color
+  Write-Host "Set $Level to $Color"
 }
 
+<#
+.DESCRIPTION
+Gets the full log file path
+.EXAMPLE
+$logFilePath = Get-LogFilePath
+#>
 function Get-LogFilePath {
   if ($script:FilePath) {
     return $script:FilePath
@@ -32,7 +53,7 @@ function Get-LogFilePath {
 function Get-LinePrefix {
   param (
       [Parameter(Position=0)][datetime]$dateTime=(Get-Date),
-      [Parameter(Position=1)][string]$tag="INFO"
+      [Parameter(Position=1)][string]$Level="INFO"
   )
 
   return "[$($dateTime.ToString("yyyyMMddHHmmss"))][$tag]"
@@ -101,27 +122,82 @@ function Create-LogFile {
 #endregion
 
 #region public functions
+<#
+.DESCRIPTION
+Writes INFO level message to log file and write-host
+
+.PARAMETER Message
+The message to log
+
+.EXAMPLE
+Log-Info "Something waranting info happened!"
+
+.NOTES
+Default write-host in WHITE. Can be set using Set-LevelTextColor
+#>
 function Log-Info {
   param (
     [Parameter(Mandatory=$true, Position=0)][string]$Message
   )
   Write-Log $Message "INFO"
+  # Write-Information $Message
 }
 
+<#
+.DESCRIPTION
+Writes WARN level message to log file and write-host
+
+.PARAMETER Message
+The message to log
+
+.EXAMPLE
+Log-Warn "Something waranting WARN happened!"
+
+.NOTES
+Default write-host in YELLOW. Can be set using Set-LevelTextColor
+#>
 function Log-Warn {
   param (
     [Parameter(Mandatory=$true, Position=0)][string]$Message
   )
   Write-Log $Message "WARN"
+  # Write-Warning $Message
 }
 
+<#
+.DESCRIPTION
+Writes ERROR level message to log file and write-host
+
+.PARAMETER Message
+The message to log
+
+.EXAMPLE
+Log-Error "Something waranting ERROR happened!"
+
+.NOTES
+Default write-host in RED. Can be set using Set-LevelTextColor
+#>
 function Log-Error {
   param (
     [Parameter(Mandatory=$true, Position=0)][string]$Message
   )
   Write-Log $Message "ERROR"
+  # Write-Error $Message
 }
 
+<#
+.DESCRIPTION
+Writes DEBUG level message to log file and write-host
+
+.PARAMETER Message
+The message to log
+
+.EXAMPLE
+Log-Debug "Something waranting DEBUG happened!"
+
+.NOTES
+Default write-host in CYAN. Can be set using Set-LevelTextColor
+#>
 function Log-Debug {
   param (
     [Parameter(Mandatory=$true, Position=0)][string]$Message
@@ -129,13 +205,58 @@ function Log-Debug {
   if ($script:isDebug) {
     Write-Log $Message "DEBUG"
   }
+  Write-Debug $Message
 }
 
+<#
+.DESCRIPTION
+Creates the log file. This includes creating any directories listed in the -LogPath that don't 
+exist and creates the log file name. File name creation includes getting the datetime to add
+before the file extension.
+
+.PARAMETER LogPath
+The full path of where the log file will exist. If "C:\log\lumberjack.log" is passed, then the 
+"log" directory will be created. By default, the file name created in this example would result
+in a file name "lumberjack-yyyyMMddHHmmss.log" where "yyyyMMddHHmmss" would be the datetime in 
+that format.
+
+Default: "$env:TEMP\PoShLumberjack\log\Lumberjack.log"
+
+.PARAMETER LogNote
+A normal log-Info entry with an added [NOTE] tag just in case you have something of note you 
+want to add. Might be good if you keep a rolling log.
+
+Will show up like this:
+========================
+[yyyyMMddHHmmss][INFO][NOTE] My important note about the thing this is doing
+========================
+
+.PARAMETER DateFormat
+The format in which you want the date to appear in the file name
+
+Default: "yyyyMMddHHmmss"
+Example: "yyMMdd"
+Doc: https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+
+.PARAMETER IsDebug
+By default, debug logging will not write to host or file. Pass the -IsDebug switch to get Log-Debug entries to show
+
+.PARAMETER NoDateTime
+If you don't want the log file name to be modified with the date, use the -NoDateTime switch
+
+.EXAMPLE
+Start-Logger -LogPath $LogPath -LogNote $LogNote -DateFormat $DateFormat
+Start-Logger -NoDateTime
+Start-Logger -IsDebug
+
+.NOTES
+Log-Debug will only show up if you pass the -IsDebug Switch
+#>
 function Start-Logging {
   param (
     [Parameter(Position=0)][string]$LogPath="$env:TEMP\PoShLumberjack\log\Lumberjack.log",
     [Parameter(Position=1)][string]$LogNote,
-    [Parameter(Position=2)][string]$dateFormat="yyyyMMddHHmmss",
+    [Parameter(Position=2)][string]$DateFormat="yyyyMMddHHmmss",
     [Parameter(Position=3)][switch]$IsDebug=$false,
     [Parameter(Position=4)][switch]$NoDateTime=$false
 
@@ -144,13 +265,15 @@ function Start-Logging {
   Write-Verbose "Setting up log file..."
   if (-not (Test-Path $LogPath)) {
     Write-Verbose "  Creating log file: $LogPath"
-    $script:FilePath = (Create-LogFile $LogPath $dateFormat $NoDateTime).FullName
+    $script:FilePath = (Create-LogFile $LogPath $DateFormat $NoDateTime).FullName
     Write-Verbose "  Created log file: $($script:FilePath)"
   }
   $script:IsDebug = $IsDebug
   Write-Verbose "  Log file setup complete"
   if ($LogNote) {
+    Log-Info "========================"
     Log-Info "[NOTE] $LogNote"
+    Log-Info "========================"
   }
 }
 #endregion
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # PoShLumberjack
-Simple Powershell Logger
+
+[![Build status](https://ci.appveyor.com/api/projects/status/4loxtsfmwvae8cqc/branch/master?svg=true)](https://ci.appveyor.com/project/EmmanuelPineiro/poshlumberjack/branch/master)
+
+## Install PoShLumberjack
+```powershell
+PS> Install-Module PoShLumberjack
+```
+
+## Setup PoShLumberjack
+
+### Create a log file with no args
+No arguments are needed to create a log file. This will:
+  - use a default dir
+  - default dateTime format
+  - not show Log-Debug
+  - create a new log file with timestamp in the name
+
+Defaults:
+  - LogPath: "$env:TEMP\PoShLumberjack\log\Lumberjack.log"
+  - DateFormat: "yyyyMMddHHmmss"
+
+```powershell
+Start-Logger
+```
+
+### Create a new time stamped log each time
+```powershell
+# Specify where your logs are created, and how the date time gets added before the extension
+$LogPath = 'C:\logs\lumberjack.log'
+$DateFormat = 'yyyyMMdd-HH:mm:ss'
+Start-Logger -LogPath $LogPath -LogNote $LogNote -DateFormat $DateFormat
+
+# Default colors are INFO='White'; WARN='Yellow'; ERROR='Red'; DEBUG='Cyan'. They can be changed using the Set-LevelTextColor command. 
+Set-LevelTextColor "INFO" "DarkMagenta"
+```
+
+### Create a new log file with no time stamp
+```powershell
+# If you don't want the log file name to be modified with the date, use the -NoDateTime switch
+Start-Logger -NoDateTime
+```
+
+### Create a log file and include Log-Debug calls
+```powershell
+# By default, debug logging will not write to host or file. Pass the -IsDebug switch to get Log-Debug entries to show
+Start-Logger -IsDebug
+```
+
+## Using PoShLumberjack
+```powershell
+# Logging will write to file and write-host in color
+Log-Info "an info message"
+Log-Debug "a debug message"
+Log-Warn "a warn message"
+Log-Error "an error message"
+```
